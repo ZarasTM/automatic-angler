a93af6997fa7bdeb6f9685c37b2f74f87fdf3987
ZarasTM <p.zaras@wp.pl>
2019-03-27 16:32:35 +0100

    minor fixes

--- src/ScreenScrapper.py
+++ src/ScreenScrapper.py
@@ -1,4 +1,5 @@
-import PIL.ImageGrab
+from PIL import Image, ImageGrab
+import math
 
 class ScreenScrapper:
     # Screen/Game resolution
@@ -12,20 +13,30 @@ class ScreenScrapper:
     currFrame = []
 
     def __init__(self):
-        self.screenRes = PIL.ImageGrab.grab().size
+        self.screenRes = ImageGrab.grab().size
         self.calcGameWindow()
         self.updateFrame()
 
     # Calculates all data needed for operating with game itself
     def calcGameWindow(self):
-        self.gameRes[0] = self.screenRes[1]
+        self.gameRes[0] = int((72*self.screenRes[1])/66)
         self.gameRes[1] = self.screenRes[1]
-        self.margin = (self.screenRes[0] - self.gameRes[0]) / 2
+        self.margin = int((self.screenRes[0] - self.gameRes[0]) / 2)
 
     # Updates game frame
     def updateFrame(self):
-        self.currFrame = PIL.ImageGrab.grab(bbox=(
+        self.currFrame = ImageGrab.grab(bbox=(
             self.margin,
             0,
             self.margin+self.gameRes[0],
             self.gameRes[1])).load()
+
+    # Saves image to a tmp.png file
+    def saveImg(self):
+        pix = []
+        for col in range(self.gameRes[1]):
+            for row in range(self.gameRes[0]):
+                pix.append(self.currFrame[row, col])
+        im = Image.new("RGB", (self.gameRes[0], self.gameRes[1]))
+        im.putdata(pix)
+        im.save("tmp.png")
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from ScreenScrapper import ScreenScrapper
+import win32api, win32con, time
+
+time.sleep(5)
 
 # Object to get screen data
 scr = ScreenScrapper()
+scr.saveImg()
 
 # State of a game
 state = 0
 
 def mainLoop():
+    while true:
+        scr.updateFrame()
