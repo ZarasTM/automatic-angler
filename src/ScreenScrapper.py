9958e77594d1db08e934d2df749b32814c608b0a
ZarasTM <p.zaras@wp.pl>
2019-03-27 13:48:51 +0100

    Basic screen recorder

--- src/ScreenScrapper.py
+++ src/ScreenScrapper.py
@@ -1,18 +1,31 @@
-import pyautogui
+import PIL.ImageGrab
 
 class ScreenScrapper:
-    # Screen resolution
+    # Screen/Game resolution
     screenRes = [0, 0]
-
-    # Game resolution
     gameRes = [0, 0]
 
+    # Margin of game window
+    margin = 0
+
+    # Current game frame
+    currFrame = []
+
     def __init__(self):
-        self.screenRes = pyautogui.size()
+        self.screenRes = PIL.ImageGrab.grab().size
         self.calcGameWindow()
+        self.updateFrame()
 
     # Calculates all data needed for operating with game itself
     def calcGameWindow(self):
         self.gameRes[0] = self.screenRes[1]
         self.gameRes[1] = self.screenRes[1]
-        print(self.gameRes)
+        self.margin = (self.screenRes[0] - self.gameRes[0]) / 2
+
+    # Updates game frame
+    def updateFrame(self):
+        self.currFrame = PIL.ImageGrab.grab(bbox=(
+            self.margin,
+            0,
+            self.margin+self.gameRes[0],
+            self.gameRes[1])).load()
--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 from ScreenScrapper import ScreenScrapper
 
+# Object to get screen data
 scr = ScreenScrapper()
+
+# State of a game
+state = 0
+
+def mainLoop():
