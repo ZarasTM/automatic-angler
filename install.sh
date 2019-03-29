f1f3ad6188c3ca13bfd26de296bfac697d1940ee
ZarasTM <p.zaras@wp.pl>
2019-03-29 17:15:48 +0100

    Tweaks to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/sh
 
-if type python -v &> /dev/null then	echo "Couldn't find Python on your computer..."
-if type pip &> /dev/null then echo "Couldn't find pipi on your computer..."
+if type python -v &> /dev/null ; then
+	echo "Couldn't find Python on your computer..." 
+	read -n1 -r -p "Press any key to continue..." key
+	exit 0
+fi
 
-pip install pynput
-pip install pillow
+if type pip -v &> /dev/null ; then 
+	echo "Couldn't find pip on your computer..." 
+	read -n1 -r -p "Press any key to continue..." key
+	exit 0
+else
+	pip install pynput
+	pip install pillow
+	read -n1 -r -p "Press any key to continue..." key
+fi
